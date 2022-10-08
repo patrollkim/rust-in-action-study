09d6257f67a179a674a737049ee1ad0894dcf60f
khj <khj@kopens.com>
2022-10-08 11:01:21 +0900

    chapter2 2.5 예제

--- chapter2/example-2_5/src/main.rs
+++ chapter2/example-2_5/src/main.rs
@@ -1,3 +1,12 @@
+//use std::convert::TryInto; => rust 2021 에디션 부터 prelude 로 추가됨 
+
 fn main() {
-    println!("Hello, world!");
+    let a: i32 = 10;
+    let b: u16 = 100;
+
+    let b_ = b.try_into().unwrap();
+
+    if a < b_ {
+        println!("10 is less than 100.");
+    }
 }
