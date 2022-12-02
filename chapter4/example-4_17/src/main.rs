a12472de5d2d3d46aca47549732a1a2a0f698a1e
khj <khj@kopens.com>
2022-12-02 16:25:52 +0900

    chapter4 4.17 예제

---
+++ chapter4/example-4_17/src/main.rs
@@ -0,0 +1,40 @@
+#[derive(Debug)]
+struct CubeSat {
+    id: u64,
+}
+
+#[derive(Debug)]
+enum StatusMessage {
+    Ok,
+}
+
+impl Copy for CubeSat {}
+
+impl Copy for StatusMessage {}
+
+impl Clone for CubeSat {
+    fn clone(&self) -> Self {
+        CubeSat { id: self.id }
+    }
+}
+
+impl Clone for StatusMessage {
+    fn clone(&self) -> Self {
+        *self
+    }
+}
+
+
+fn check_status(sat_id: CubeSat) -> StatusMessage {
+    StatusMessage::Ok
+}
+
+fn main() {
+    let sat_a = CubeSat { id: 0 };
+    let a_status = check_status(sat_a.clone());
+    println!("a: {:?}", a_status);
+
+    let a_status = check_status(sat_a.clone());
+    println!("a: {:?}", a_status);
+}
+
