e6e9edd7be7aea8328b4eca709c0468f1900b158
khj <khj@kopens.com>
2022-12-02 16:24:38 +0900

    chapter4 4.9 예제

--- chapter4/example-4_9/src/main.rs
+++ chapter4/example-4_9/src/main.rs
@@ -1,5 +1,23 @@
 #[derive(Debug)]
+struct CubeSat {
+    id: u64,
+    mailbox: Mailbox,
+}
+
+#[derive(Debug)]
+struct Mailbox {
+    messages: Vec<Message>,
+}
+
+#[derive(Debug)]
+enum StatusMessage {
+    Ok,
+}
+
+type Message = String;
 
 fn main() {
+
+    let sat_a = CubeSat { id: 100, mailbox: Mailbox { messages: vec![] } };
     
 }
