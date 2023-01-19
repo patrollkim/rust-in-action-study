36c29759a9e9a5417b64d541a1ed970d45afaf4a
khj <khj@kopens.com>
2023-01-19 20:01:59 +0900

    chapter5 5.28 예제

---
+++ chapter5/example-5_28/src/main.rs
@@ -0,0 +1,37 @@
+struct CPU {
+    registers: [u8; 16],
+    position_in_memory: usize,
+    memory: [u8; 4096],
+    stack: [u16; 16],
+    stack_pointer: usize,
+}
+
+impl CPU {
+    fn call(&mut self, addr: u16) {
+        let sp = self.stack_pointer;
+        let stack = &mut self.stack;
+        if sp > stack.len() {
+            panic!("Stack Overflow!");
+        }
+
+        stack[sp] = self.position_in_memory as u16;
+        self.stack_pointer += 1;
+        self.position_in_memory = addr as usize;
+    }
+
+    fn ret(&mut self) {
+        if self.stack_pointer == 0 {
+            panic!("Stack Underflow!");
+        }
+
+        self.stack_pointer -= 1;
+        let addr = self.stack[self.stack_pointer];
+        self.position_in_memory = addr as usize;
+    }
+}
+
+fn main() {
+    
+}
+
+
