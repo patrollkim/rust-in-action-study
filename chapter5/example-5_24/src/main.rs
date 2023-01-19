688aa2e9a55a5c186c49ed88a940e9cf49c3999e
khj <khj@kopens.com>
2023-01-19 20:01:30 +0900

    chapter5 5.24 예제

---
+++ chapter5/example-5_24/src/main.rs
@@ -0,0 +1,19 @@
+struct CPU {
+    registers: [u8; 16],
+    position_in_memory: usize,
+    memory: [u8; 0x1000],
+}
+
+impl CPU {
+    fn read_opcode(&self) -> u16 {
+        let p = self.position_in_memory;
+        let op_byte1 = self.memory[p] as u16;
+        let op_byte2 = self.memory[p + 1] as u16;
+
+        op_byte1 << 8 | op_byte2
+    }
+}
+
+fn main() {
+    
+}
