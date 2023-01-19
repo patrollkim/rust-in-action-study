3e2e323337b25f8b5aebef2cffb512bf25cb55f1
khj <khj@kopens.com>
2023-01-19 20:01:37 +0900

    chapter5 5.25 예제

---
+++ chapter5/example-5_25/src/main.rs
@@ -0,0 +1,33 @@
+struct CPU {
+    registers: [u8; 16],
+    position_in_memory: usize,
+    memory: [u8; 0x1000],
+}
+
+impl CPU {
+    fn read_opcode(&self) -> u16 {
+        let p = self.position_in_memory;
+        let op_byte1 = self.memory[p] as u16;
+        let op_byte2 = self.memory[p + 1] as u16;
+
+        op_byte1 << 8 | op_byte2
+    }
+
+    fn add_xy(&mut self, x: u8, y: u8) {
+        let arg1 = self.registers[x as usize];
+        let arg2 = self.registers[y as usize];
+
+        let (val, overflow) = arg1.overflowing_add(arg2);
+        self.registers[x as usize] = val;
+
+        if overflow {
+            self.registers[0xF] = 1
+        } else {
+            self.registers[0xF] = 0;
+        }
+    }
+}
+
+fn main() {
+    
+}
