8156d9c4c4bfd8e66c7f2473e814087316a181bb
khj <khj@kopens.com>
2023-01-19 20:01:11 +0900

    chapter5 5.21 예제

---
+++ chapter5/example-5_21/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    let opcode: u16 = 0x71E4;
+
+    let c = (opcode & 0xF000) >> 12;
+    let x = (opcode & 0x0F00) >> 8;
+    let y = (opcode & 0x00F0) >> 4;
+    let d = (opcode & 0x000F) >> 0;
+
+    assert_eq!(c, 0x7);
+    assert_eq!(x, 0x1);
+    assert_eq!(y, 0xE);
+    assert_eq!(d, 0x4);
+
+    let nnn = opcode & 0x0FFF;
+    let kk = opcode & 0x00FF;
+    
+    assert_eq!(nnn, 0x1E4);
+    assert_eq!(kk,   0xE4);
+}
